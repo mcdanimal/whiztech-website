8fc320861d0e7d4d45fb35914e903276d77a6081
mcdanimal <dan.mccabe@gmail.com>
2026-01-19 19:03:42 -0500

    Added email logic; installed flask-mail and python-dotenv

--- app_submit.py
+++ app_submit.py
@@ -1,6 +1,21 @@
+import os
 from flask import Flask, render_template, request
+from flask_mail import Mail, Message
+from dotenv import load_dotenv
+
+load_dotenv()
 
 app = Flask(__name__)
+
+app.config['MAIL_SERVER'] = 'smtp.gmail.com'
+app.config['MAIL_PORT'] =587
+app.config['MAIL_USE_TLS'] = True
+
+app.config['MAIL_USERNAME'] = os.getenv('MAIL_USERNAME')
+app.config['MAIL_PASSWORD'] = os.getenv('MAIL_PASSWORD')
+app.config['MAIL_DEFAULT_SENDER'] = os.getenv('MAIL_SENDER')
+
+mail = Mail(app)
 
 @app.route('/')
 def home():
@@ -16,13 +31,26 @@ def submit():
 
     full_name = customer_fname + " " + customer_lname
 
-    #*************JUST FOR DEV TEST - SEND TO DB AND EMAIL LATER***************
-    print("--------NEW SERVICE REQUEST--------")
-    print(f"NAME: {full_name}")
-    print(f"EMAIL: {customer_email}")
-    print(f"SERVICE: {service_type}")
-    print(f"DETAILS: {details}")
-    print("----------------END----------------")
+    subject = f"New Service Request from {full_name}"
+    body = f"""
+    You have receieved a new request!"
+
+    Customer: {full_name}
+    Email: {customer_email}
+    Service Type: {service_type}
+
+    Details:
+    {details}
+    """
+
+    msg = Message(subject, recipients=[os.getenv("MAIL_SENDER")])
+    msg.body = body
+
+    try:
+        mail.send(msg)
+        print("Email sent successfully!")
+    except Exception as e:
+        print(f"Failed to send email: {e}")
 
     return render_template('submit_success.html', name_in_html=full_name)
 

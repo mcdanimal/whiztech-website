8baaf176e3f16673a6cbb570d26f3a0514190303
mcdanimal <dan.mccabe@gmail.com>
2026-01-23 07:39:04 -0500

    Switched to SSL 465 for email to fix timeout

--- app_submit.py
+++ app_submit.py
@@ -8,8 +8,9 @@ load_dotenv()
 app = Flask(__name__)
 
 app.config['MAIL_SERVER'] = 'smtp.gmail.com'
-app.config['MAIL_PORT'] =587
-app.config['MAIL_USE_TLS'] = True
+app.config['MAIL_PORT'] =465
+app.config['MAIL_USE_TLS'] = False
+app.config['MAIL_USE_SSL'] = True
 
 app.config['MAIL_USERNAME'] = os.getenv('MAIL_USERNAME')
 app.config['MAIL_PASSWORD'] = os.getenv('MAIL_PASSWORD')
